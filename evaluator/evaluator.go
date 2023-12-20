fe4f62c8ecf43024c5753362f42fde13f40ec1cd
dezzer <baev2m@gmail.com>
2023-12-20 14:56:16 +0500

    feat: if else expression eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -32,8 +32,13 @@ func Eval(node ast.Node) object.Object {
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
 		right := Eval(node.Right)
-
 		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -130,5 +135,30 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ifEx *ast.IfExpression) object.Object {
+	condition := Eval(ifEx.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ifEx.Consequence)
+	} else if ifEx.Alternative != nil {
+		return Eval(ifEx.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
